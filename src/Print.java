9991f582e7a76c251169ff4d2ce38de99ff4b563
bwilmes <nadnerb97@gmail.com>
2014-03-13 13:23:39 -0400

    Commit

---
+++ src/Print.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+import java.awt.print.*;
+import javax.print.*;
+import javax.print.attribute.*;
+public class Print extends AbstractPrint {
+	PrinterJob printWindow;
+	MainScreen mainWindow;
+	Object printThing;
+	
+	public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
+		try {
+			if (printWindow.printDialog()){
+				PrintService printer=printWindow.getPrintService();
+				DocPrintJob docJob=printer.createPrintJob();
+				DocFlavor flavor=new DocFlavor("text/plain","java.io.InputStream");
+				printWindow.print();
+				SimpleDoc paper=new SimpleDoc(printThing,flavor,new HashDocAttributeSet());
+				docJob.print(paper, new HashPrintRequestAttributeSet());
+			}	
+		} catch(PrinterException | PrintException p){
+			p.printStackTrace();
+			return NO_SUCH_PAGE;
+		}
+		return PAGE_EXISTS;
+	}
+	
+	public Print(MainScreen window,Object subject){
+		mainWindow=window;
+		printThing=
+		printWindow=PrinterJob.getPrinterJob();
+	}
+}
