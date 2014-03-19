d6a6d96da1656b55ddae18fcf89b2907f3c90d8c
nfsmith <nickfsmith96@gmail.com>
2014-03-19 14:07:28 -0400

    commit
    `

--- src/EventPanel.java
+++ src/EventPanel.java
@@ -1,6 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Point;
@@ -32,8 +33,7 @@ import javax.swing.SwingConstants;
 import javax.swing.event.PopupMenuEvent;
 import javax.swing.event.PopupMenuListener;
 
-public class EventPanel extends AbstractEventPanel implements MouseListener,
-		ActionListener {
+public class EventPanel extends AbstractEventPanel implements MouseListener,ActionListener {
 	private MainScreen mainWindow;
 	private ArrayList<String> events;
 	private ArrayList<SubEventPanel> eventPanels;
@@ -41,6 +41,9 @@ public class EventPanel extends AbstractEventPanel implements MouseListener,
 	private Point mouse;
 	private JPopupMenu popup;
 	private SubEventPanel mover;
+	private JMenuItem deleteItem;
+	private JMenuItem completeItem;
+	private JMenuItem editItem;
 
 	// JScrollPane
 	EventPanel(MainScreen mw) {
@@ -50,27 +53,28 @@ public class EventPanel extends AbstractEventPanel implements MouseListener,
 		panel.setLayout(new GridLayout(0, 1));
 		addMouseListener(this);
 		popup = new JPopupMenu();
-		JMenuItem menuItem = new JMenuItem("Delete Item");
-		menuItem.addActionListener(this);
-		popup.add(menuItem);
-		menuItem = new JMenuItem("Complete Item");
-		menuItem.addActionListener(this);
-		popup.add(menuItem);
-		menuItem = new JMenuItem("Edit Item");
-		menuItem.addActionListener(this);
-		popup.add(menuItem);
+		deleteItem = new JMenuItem("Delete Item");
+		deleteItem.addActionListener(this);
+		popup.add(deleteItem);
+		completeItem = new JMenuItem("Complete Item");
+		completeItem.addActionListener(this);
+		popup.add(completeItem);
+		editItem = new JMenuItem("Edit Item");
+		editItem.addActionListener(this);
+		popup.add(editItem);
 		MouseListener popupListener = new PopupListener();
 		addMouseListener(popupListener);
-		setPreferredSize(new Dimension(mainWindow.getHeight(),mainWindow.getWidth()));
-		
+		setPreferredSize(new Dimension(mainWindow.getHeight(),
+				mainWindow.getWidth()));
+
 		for (int i = 0; i < mw.si.getEventList().size(); i++) {
 			eventPanels.add(new SubEventPanel(mw.si.getEventList().get(i)));
 			panel.add(eventPanels.get(i));
 		}
-//		 for (int i = 0; i < 200; i++) {
-//			 eventPanels.add(new SubEventPanel(new Event("Event "+i)));
-//			 panel.add(eventPanels.get(i));
-//		 }
+		// for (int i = 0; i < 200; i++) {
+		// eventPanels.add(new SubEventPanel(new Event("Event "+i)));
+		// panel.add(eventPanels.get(i));
+		// }
 		setViewportView(panel);
 
 	}
@@ -106,15 +110,7 @@ public class EventPanel extends AbstractEventPanel implements MouseListener,
 	}
 
 	private void movePanel(SubEventPanel mover, Point mousePoint) {
-		double scrollBar = getVerticalScrollBar().getValue();
-		double eventPanelsHeight = eventPanels.get(0).getHeight();
-		getVerticalScrollBar().setValue(getVerticalScrollBar().getMaximum());
-		double maxScroll = getVerticalScrollBar().getValue();
-		getVerticalScrollBar().setValue((int) scrollBar);
-		double verticalShiftPercent = (scrollBar / maxScroll);
-		int verticalShift = (int) (verticalShiftPercent * (eventPanels.size() - 48));
-		Point actualPoint = new Point((int) mousePoint.getX(),
-				(int) (mousePoint.getY() + verticalShift * eventPanelsHeight));
+		Point actualPoint = actualPoint(mousePoint);
 		for (int i = 0; i < eventPanels.size(); i++) {
 			SubEventPanel test = eventPanels.get(i);
 			SubEventPanel test2 = eventPanels.get(i);
@@ -137,7 +133,7 @@ public class EventPanel extends AbstractEventPanel implements MouseListener,
 	}
 
 	private void sort(SubEventPanel mover, int newSpot) {
-		System.out.println("New Spot " + newSpot);
+		// System.out.println("New Spot " + newSpot);
 		ArrayList<SubEventPanel> newList = new ArrayList<SubEventPanel>();
 		for (int i = 0; i <= newSpot; i++) {
 			if (eventPanels.get(i) != mover) {
@@ -145,13 +141,14 @@ public class EventPanel extends AbstractEventPanel implements MouseListener,
 			}
 		}
 		newList.add(mover);
-		System.out.println(newList.get(0).getText());
+		// System.out.println(newList.get(0).getText());
 		for (int i = newSpot + 1; i < eventPanels.size(); i++) {
 			if (eventPanels.get(i) != mover) {
 				newList.add(eventPanels.get(i));
 			}
 		}
 		eventPanels = newList;
+		// mainWindow.si.updateEventList()
 		panel.removeAll();
 		for (int i = 0; i < eventPanels.size(); i++) {
 			panel.add(eventPanels.get(i));
@@ -163,15 +160,38 @@ public class EventPanel extends AbstractEventPanel implements MouseListener,
 	}
 
 	private class SubEventPanel extends JLabel {
-		SubEventPanel(AbstractEvent e) {
+		private Event event;
+		SubEventPanel(Event e) {
+			event=e;
 			setText(e.getName());
 			setHorizontalAlignment(SwingConstants.CENTER);
 			setPreferredSize(getMinimumSize());
+			if (e.getPriority() == 0) {
+			} else if (e.getPriority() == 1) {
+				setFont(this.getFont().deriveFont(Font.ITALIC));
+			} else if (e.getPriority() == 2) {
+				setFont(this.getFont().deriveFont(Font.ITALIC));
+			} else if (e.getPriority() == 3) {
+			} else if (e.getPriority() == 4) {
+				setFont(this.getFont().deriveFont(Font.BOLD));
+			} else {
+				JFrame error = new JFrame("ERROR");
+				error.add(new JLabel(
+						"Priority Error, input priority does not match any"));
+				error.setSize(new Dimension(300, 300));
+				error.setVisible(true);
+				error.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			}
 		}
+		private Event getEvent(){
+			return event;
+		}
+
 		SubEventPanel(String s) {
 			setText(s);
 			setHorizontalAlignment(SwingConstants.CENTER);
 		}
+
 	}
 
 	public void mouseClicked(MouseEvent arg0) {
@@ -190,6 +210,7 @@ public class EventPanel extends AbstractEventPanel implements MouseListener,
 		private void maybeShowPopup(MouseEvent e) {
 			if (e.isPopupTrigger()) {
 				popup.show(e.getComponent(), e.getX(), e.getY());
+				mouse=e.getPoint();
 			}
 		}
 	}
@@ -210,23 +231,13 @@ public class EventPanel extends AbstractEventPanel implements MouseListener,
 		Point mouse = arg0.getPoint();
 		if (arg0.getButton() == MouseEvent.BUTTON1) {
 			if (eventPanels.size() > 0) {
-				double scrollBar = getVerticalScrollBar().getValue();
-				double eventPanelsHeight = eventPanels.get(0).getHeight();
-				getVerticalScrollBar().setValue(
-						getVerticalScrollBar().getMaximum());
-				double maxScroll = getVerticalScrollBar().getValue();
-				getVerticalScrollBar().setValue((int) scrollBar);
-				double verticalShiftPercent = (scrollBar / maxScroll);
-				int verticalShift = (int) (verticalShiftPercent * (eventPanels
-						.size() - 48));
-				Point actualPoint = new Point(
-						(int) mouse.getX(),
-						(int) (mouse.getY() + verticalShift * eventPanelsHeight));
+				Point actualPoint = actualPoint(mouse);
 				mover = (SubEventPanel) panel.getComponentAt(eventPanels.get(0)
 						.getX() + 5, (int) actualPoint.getY());
 			}
 		}
 	}
+
 	public void mouseReleased(MouseEvent arg0) {
 		if (mover != null) {
 			movePanel(mover, arg0.getPoint());
@@ -235,18 +246,31 @@ public class EventPanel extends AbstractEventPanel implements MouseListener,
 		mainWindow.setVisible(true);
 	}
 
-	public void actionPerformed(ActionEvent arg0) {
-	}
-	
-	public void refresh(){
-		eventPanels.removeAll(eventPanels);
+	public void actionPerformed(ActionEvent event) {
+		if (event.getSource().equals(deleteItem)){
+			Point actualPoint=actualPoint(mouse);
+			Event remove =((SubEventPanel) panel.getComponentAt(actualPoint)).getEvent();
+			mainWindow.si.getEventList().remove(remove);
+			refresh();
+		} else if (event.getSource().equals(completeItem)){
+			Point actualPoint=actualPoint(mouse);
+			Event complete =((SubEventPanel) panel.getComponentAt(actualPoint)).getEvent();
+			complete.setPriority(Event.CLOSED);
+		} else if (event.getSource().equals(editItem)){
+		}
 		
+	}
+
+	public void refresh() {
+		eventPanels.removeAll(eventPanels);
+
 		panel.removeAll();
 		for (int i = 0; i < mainWindow.si.getEventList().size(); i++) {
-			Event e=mainWindow.si.getEventList().get(i);
-			if(e.getPriority()==e.INACTIVE){
-				Calendar[] c=e.getDates();
-				DateFormat date=new SimpleDateFormat("MM/DD/YYYY G 'at' HH:mm:ss z");
+			Event e = mainWindow.si.getEventList().get(i);
+			if (e.getPriority() == e.INACTIVE) {
+				Calendar[] c = e.getDates();
+				DateFormat date = new SimpleDateFormat(
+						"MM/DD/YYYY G 'at' HH:mm:ss z");
 				String s = date.format(c[0]);
 				eventPanels.add(new SubEventPanel(s));
 			}
@@ -257,6 +281,21 @@ public class EventPanel extends AbstractEventPanel implements MouseListener,
 		}
 		mainWindow.setVisible(false);
 		mainWindow.setVisible(true);
+	}
+	private Point actualPoint(Point mouse){
+		double scrollBar = getVerticalScrollBar().getValue();
+		double eventPanelsHeight = eventPanels.get(0).getHeight();
+		getVerticalScrollBar().setValue(
+				getVerticalScrollBar().getMaximum());
+		double maxScroll = getVerticalScrollBar().getValue();
+		getVerticalScrollBar().setValue((int) scrollBar);
+		double verticalShiftPercent = (scrollBar / maxScroll);
+		int verticalShift = (int) (verticalShiftPercent * (eventPanels
+				.size() - 48));
+		Point actualPoint = new Point(
+				(int) mouse.getX(),
+				(int) (mouse.getY() + verticalShift * eventPanelsHeight));
+		return actualPoint;
 	}
 
 }
