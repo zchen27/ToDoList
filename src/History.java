65d6a7b3685568c5cc3bdb12983e9279dc11cac3
zchen27 <zchen27@gmail.com>
2014-03-21 14:08:55 -0400

    JAXB

--- src/History.java
+++ src/History.java
@@ -1,3 +1,15 @@
 import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.*;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlSeeAlso({HistoryEntry.class})
 public class History extends ArrayList <HistoryEntry> {
+	
+	@XmlElement(name = "event")
+	public List<HistoryEntry> getEventList()
+	{
+		return this;
+	}
 }
